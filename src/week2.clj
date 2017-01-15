637d605e5214387dcc730bde527c610755009397
Johannes Staffans <johannes.staffans@gmail.com>
2017-01-15 13:04:51 +0100

    Count inversions

--- src/week2.clj
+++ src/week2.clj
@@ -8,27 +8,30 @@
 (def ints
   (memoize ints*))
 
-(defn merge-sort [coll]
+(defn merge-sort-and-count-inversions [coll]
   (if (= 1 (count coll))
-    coll
-    (let [[left right] (split-at (/ (count coll) 2) coll)]
-      (loop [acc []
-             ll (merge-sort left)
-             rr (merge-sort right)]
+    [coll 0]
+    (let [[left right]      (split-at (/ (count coll) 2) coll)
+          [sorted-left il]  (merge-sort-and-count-inversions left)
+          [sorted-right ir] (merge-sort-and-count-inversions right)]
+      (loop [acc        []
+             ll         sorted-left
+             rr         sorted-right
+             inversions (+ il ir)]
         (cond
           (empty? ll)
-          (into acc rr)
+          [(into acc rr) inversions]
 
           (empty? rr)
-          (into acc ll)
+          [(into acc ll) inversions]
 
           :else
           (if (< (first ll) (first rr))
-            (recur (conj acc (first ll)) (rest ll) rr)
-            (recur (conj acc (first rr)) ll (rest rr))))))))
+            (recur (conj acc (first ll)) (rest ll) rr inversions)
+            (recur (conj acc (first rr)) ll (rest rr) (+ inversions (count ll)))))))))
 
-(merge-sort [4 1 99 6 23 8])
+(println (second (merge-sort-and-count-inversions (ints "resources/IntegerArray.txt"))))
 
-;; (take 10 (merge-sort (take 5000 (ints "resources/IntegerArray.txt"))))
+
 
 ;; (time (merge-sort (ints "resources/IntegerArray.txt")))
